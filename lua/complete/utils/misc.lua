e1a3f26aed85444827ccb108025f87624dd39f92
LongyuXie <longyuxie266@gmail.com>
2024-01-05 16:05:33 +0800

    ✨ feat(misc): add  concatArray

--- lua/complete/utils/misc.lua
+++ lua/complete/utils/misc.lua
@@ -3,7 +3,7 @@ local misc = {}
 misc.set_window_option = function (win, name, value)
     local eventignore = vim.opt.eventignore:get()
     vim.opt.eventignore:append('OptionSet')
-    vim.api.nvim_win_set_option(window, name, value)
+    vim.api.nvim_win_set_option(win, name, value)
     vim.opt.eventignore = eventignore
 end
 
@@ -24,7 +24,14 @@ misc.deepcopy = function (tbl)
         copy[key] = misc.deepcopy(value)
     end
     return copy
+end
 
+---@param a table
+---@param b table
+misc.concatArray =  function (a, b)
+    local result = {unpack(a)}
+    table.move(b, 1, #b, #result + 1, result)
+    return result
 end
 
 return misc
